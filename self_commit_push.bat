823d1970881ddcb0e0d9b07088a08c350b697d87
Kovac Arpad <karpad2@gmail.com>
2021-01-27 19:39:14 +0100

    Auto commit +datestr

--- self_commit_push.bat
+++ self_commit_push.bat
@@ -1,7 +1,8 @@
+set datestr=%date%
 echo "self commit & push"
 
 mysqldump --opt -u root esp32project > ./db/esp32project.sql
 
 git add -A
-git commit -a -m "Auto commit"
+git commit -a -m "Auto commit "+datestr
 git push
