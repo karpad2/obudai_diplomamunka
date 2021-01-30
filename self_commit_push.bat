08b1dd156f2ac81f50e354c8b80ae829364ff6ff
Kovac Arpad <karpad2@gmail.com>
2021-01-30 12:33:38 +0100

    Auto commit

--- self_commit_push.bat
+++ self_commit_push.bat
@@ -3,7 +3,7 @@ echo "self commit & push"
 mysqldump --opt -u root esp32project > ./db/esp32project.sql
 
 pandoc -s index.docx -t markdown -o README.md
-pandoc -s index.docx -t markdown -o README.pdf
+pandoc -s index.docx -t markdown --pdf-engine=xelatex -o README.pdf
 git add -A
 git commit -a -m "Auto commit "
 git push
