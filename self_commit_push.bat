3557a1843edd371a626aef66c07cfacad25858d8
Kovac Arpad <karpad2@gmail.com>
2021-02-01 13:15:01 +0100

    Auto commit

--- self_commit_push.bat
+++ self_commit_push.bat
@@ -2,6 +2,7 @@ echo "self commit & push"
 
 mysqldump --opt -u root esp32project > ./db/esp32project.sql
 
+pandoc --extract-media ./wiki/images -s index.docx -t markdown -o index.md
 pandoc --extract-media ./wiki/images -s index.docx -t markdown -o README.md
 pandoc -s index.docx -t  --pdf-engine=xelatex -o README.pdf
 git add -A
