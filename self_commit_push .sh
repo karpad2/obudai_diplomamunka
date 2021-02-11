711e015de6374d8e907b9a15547cf673dffcd0b0
Kovács Árpád <karpad2@gmail.com>
2021-02-12 00:37:08 +0100

    Auto commit

--- self_commit_push .sh
+++ self_commit_push .sh
@@ -2,7 +2,7 @@ echo "self commit & push"
 
 mysqldump --opt -u root esp32project > ./db/esp32project.sql
 cp ./server_part/config.php ./server_part/config_sample.php 
-pandoc --extract-media ./docs/images -s index.docx -t markdown -o readme.md
+pandoc --extract-media ./docs/images -s index.docx -t markdown -o README.md
 pandoc --extract-media ./docs/images  -s index.docx -o ./docs/index.html --metadata title="Diplomamunka"
 pandoc -s index.docx -t  --pdf-engine=xelatex -o README.pdf
 git add -A
