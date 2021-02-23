a2fdebe859dc672034cc334456c11d8eeed6638b
Kovács Árpád <karpad2@gmail.com>
2021-02-23 22:29:18 +0100

    github fix

--- server_part/parts/run.php
+++ server_part/parts/run.php
@@ -1,22 +1,19 @@
 <?php
 echo '<div class="row"><h3>Run</h3></div>';
-
 if (isset($_GET["run_mod"]))
 {
+    //var_dump($_GET);
     switch ($_GET["run_mod"])
     {
-        case "start":start();
+        case "start": start();
         break;
-        case "edit" :edit();
+        case "edit" : edit();
         break;
-        case "stop" :stop();
+        case "stop" : stop();
         break;
 
         default: defa();
     }
-
-
-
 }
 if(isset($_GET["start"]))
 {
@@ -35,7 +32,7 @@ function start()
 }
 function edit()
 {
-
+echo "<a href=\"index.php?mod=run&run_mod=edit&room_id={$_GET["room_id"]}}\" class=\"w-100 btn btn-lg btn-outline-primary\">Start!</a>";
 
 
 }
@@ -43,5 +40,3 @@ function stop()
 {
 
 }
-
-
