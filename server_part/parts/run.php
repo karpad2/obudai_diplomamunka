69211879fec45849ad7f2781ba3cc6e8a30f2ed8
karpad2 <karpad2@gmail.com>
2021-02-24 11:42:31 +0100

    sir

--- server_part/parts/run.php
+++ server_part/parts/run.php
@@ -23,7 +23,7 @@ function defa()
 function start()
 {
 
-    echo "<a href=\"index.php?mod=run&run_mod=start&room_id={$_GET["room_id"]}}\" class=\"w-100 btn btn-lg btn-outline-danger\">Start!</a>";
+    echo "<a href=\"index.php?mod=run&run_mod=start&room_id={$_GET["room_id"]}}\" class=\"w-100 btn btn-lg btn-outline-success\">Start!</a>";
     echo "<a href=\"index.php?mod=run&run_mod=edit&room_id={$_GET["room_id"]}}\" class=\"w-100 btn btn-lg btn-outline-warning\">Edit!</a>";
 
 }
