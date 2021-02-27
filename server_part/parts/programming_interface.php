3018c2d4c41f2dc8f87eb6697088686dfe2e4a46
karpad <karpad2@gmail.com>
2021-02-27 17:20:45 +0100

    OwO

--- server_part/parts/programming_interface.php
+++ server_part/parts/programming_interface.php
@@ -85,25 +85,143 @@ echo "</category>";
 echo '</xml>';
 echo  '<xml xmlns="https://developers.google.com/blockly/xml" id="startBlocks" style="display: none">';
 echo '</xml>';
-
+/*
 echo '<script>
     var programmingWorkspace = Blockly.inject("blocklyDiv",
         {media: "media/",
          toolbox: document.getElementById("toolbox")});
     Blockly.Xml.domToWorkspace(document.getElementById("startBlocks"),programmingWorkspace);
-  </script>';
+  </script>';*/
 echo '</div>';
 
 echo '<div class="row">
 <form method="post" action="#" id="f-submit">
 <input type="hidden" name="program" id="program_block">
-<input type="submit" class="btn btn-primary" >
+<input type="submit" class="btn btn-primary" value="Save">
 </form>
+<input type="button" id="stepButton" />
 <script>
 function a(l)
 {
     return document.getElementById(l);
 }
+a("f-submit").onsubmit=(e)=>
+{
+    e.preventDefault();
+}
+
+var demoWorkspace = Blockly.inject("blocklyDiv",
+        {media: "media/",
+         toolbox: document.getElementById("toolbox")});
+    Blockly.Xml.domToWorkspace(document.getElementById("startBlocks"),
+        demoWorkspace);
+
+    var outputArea = document.getElementById("output");
+    var stepButton = document.getElementById("stepButton");
+    var myInterpreter = null;
+
+    function initApi(interpreter, globalObject) {
+        // Add an API function for the alert() block, generated for "text_print" blocks.
+        interpreter.setProperty(globalObject, "alert",
+            interpreter.createNativeFunction(function(text) {
+                text = arguments.length ? text : "";
+                outputArea.value += "\n" + text;
+            }));
+
+        // Add an API function for the prompt() block.
+        var wrapper = function(text) {
+            return interpreter.createPrimitive(prompt(text));
+        };
+        interpreter.setProperty(globalObject, "prompt",
+            interpreter.createNativeFunction(wrapper));
+
+        // Add an API function for highlighting blocks.
+        var wrapper = function(id) {
+            id = String(id || "");
+            return interpreter.createPrimitive(highlightBlock(id));
+        };
+        interpreter.setProperty(globalObject, "highlightBlock",
+            interpreter.createNativeFunction(wrapper));
+    }
+
+    var highlightPause = false;
+    var latestCode = "";
+
+    function highlightBlock(id) {
+        demoWorkspace.highlightBlock(id);
+        highlightPause = true;
+    }
+
+    function resetStepUi(clearOutput) {
+        demoWorkspace.highlightBlock(null);
+        highlightPause = false;
+
+        if (clearOutput) {
+            outputArea.value = "Program output:\n=================";
+        }
+    }
+
+    function generateCodeAndLoadIntoInterpreter() {
+        // Generate JavaScript code and parse it.
+        Blockly.JavaScript.STATEMENT_PREFIX = "highlightBlock(%1);\n";
+        Blockly.JavaScript.addReservedWords("highlightBlock");
+        latestCode = Blockly.JavaScript.workspaceToCode(demoWorkspace);
+        resetStepUi(true);
+    }
+
+    function stepCode() {
+        if (!myInterpreter) {
+            // First statement of this code.
+            // Clear the program output.
+            resetStepUi(true);
+            myInterpreter = new Interpreter(latestCode, initApi);
+
+            // And then show generated code in an alert.
+            // In a timeout to allow the outputArea.value to reset first.
+            setTimeout(function() {
+                alert("Ready to execute the following code\n" +
+                    "===================================\n" + latestCode);
+                highlightPause = true;
+                stepCode();
+            }, 1);
+            return;
+        }
+        highlightPause = false;
+        do {
+            try {
+                var hasMoreCode = myInterpreter.step();
+            } finally {
+                if (!hasMoreCode) {
+                    // Program complete, no more code to execute.
+                    outputArea.value += "\n\n<< Program complete >>";
+
+                    myInterpreter = null;
+                    resetStepUi(false);
+
+                    // Cool down, to discourage accidentally restarting the program.
+                    stepButton.disabled = "disabled";
+                    setTimeout(function() {
+                        stepButton.disabled = "";
+                    }, 2000);
+
+                    return;
+                }
+            }
+            // Keep executing until a highlight statement is reached,
+            // or the code completes or errors.
+        } while (hasMoreCode && !highlightPause);
+    }
+
+    // Load the interpreter now, and upon future changes.
+    generateCodeAndLoadIntoInterpreter();
+    demoWorkspace.addChangeListener(function(event) {
+        if (!event.isUiEvent) {
+            // Something changed. Parser needs to be reloaded.
+            generateCodeAndLoadIntoInterpreter();
+        }
+    });
+ 
+
 
 </script>
 </div>';
