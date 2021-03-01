182e5aebc351d2e6d4b96fb6df7e0bfd9c94c291
karpad <karpad2@gmail.com>
2021-03-01 17:27:29 +0100

    fix

--- server_part/parts/device_api.php
+++ server_part/parts/device_api.php
@@ -1,8 +1,9 @@
 <?php
 
 include "../config.php";
-if(empty($_SESSION["user"]["user_id"])) die("err 403");
-$sql="update devices set device_mode='{$_GET["device_mode"]}', status={$_GET["status"]} ".
+if(empty($_SESSION["user"]["user_id"]) or empty($_GET["device_id"]) or empty($_GET["device_mode"]) or empty($_GET["status"])) die("err 403");
+if($_GET["status"]>1) die("err 500");
+$sql="update devices set device_mode='{$_GET["device_mode"]}', status='{$_GET["status"]}' ".
     "where device_id='{$_GET["device_id"]}' and user_id='{$_SESSION["user"]["user_id"]}'";
 $res=e_sql($sql,GET_ROWS_NUMBER);
 if($res>0)
