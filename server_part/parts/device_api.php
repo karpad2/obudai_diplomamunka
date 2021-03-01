9edde992f33e13c76ce07cb53318a06a6239a1db
karpad <karpad2@gmail.com>
2021-03-01 17:21:23 +0100

    err code

--- server_part/parts/device_api.php
+++ server_part/parts/device_api.php
@@ -1,7 +1,8 @@
 <?php
-if(empty($_SESSION["user"]["user_id"])) die();
+if(empty($_SESSION["user"]["user_id"])) die("err 403");
 include "../config.php";
-$sql="update devices set device_mode='{$_GET["device_mode"]}' and status={$_GET["device_status"]} where device_id='{$_GET["device_id"]}'";
+$sql="update devices set device_mode='{$_GET["device_mode"]}', status={$_GET["status"]} ".
+    "where device_id='{$_GET["device_id"]}' and user_id='{$_SESSION["user"]["user_id"]}'";
 $res=e_sql($sql,GET_ROWS_NUMBER);
 if($res>0)
 {
