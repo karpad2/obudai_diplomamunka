27d431395d0163eba3e0b2bbc0f0d91a871d87e9
karpad <karpad2@gmail.com>
2021-03-02 11:28:01 +0100

    oh long johnson

--- server_part/parts/programming_interface.php
+++ server_part/parts/programming_interface.php
@@ -39,40 +39,7 @@ echo ' <div id="blocklyDiv" style="height: 480px;" class="col-lg"></div>';
 echo '<xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" style="display: block">';
 $sql="select * from blocks";
 $res=e_sql($sql,GET_ASSOC);
-/*
-echo '
-<category name="Logic" colour="%{BKY_LOGIC_HUE}">
-      <block type="controls_if"></block>
-      <block type="logic_compare"></block>
-      <block type="logic_operation"></block>
-      <block type="logic_negate"></block>
-      <block type="logic_boolean"></block>
-    </category>
-    <category name="Loops" colour="%{BKY_LOOPS_HUE}">
-      <block type="controls_repeat_ext">
-        <value name="TIMES">
-          <block type="math_number">
-            <field name="NUM">10</field>
-          </block>
-        </value>
-      </block>
-      <block type="controls_whileUntil"></block>
-    </category>
-    <category name="Math" colour="%{BKY_MATH_HUE}">
-      <block type="math_number">
-        <field name="NUM">123</field>
-      </block>
-      <block type="math_arithmetic"></block>
-      <block type="math_single"></block>
-    </category>
-    <category name="Text" colour="%{BKY_TEXTS_HUE}">
-      <block type="text"></block>
-      <block type="text_length"></block>
-      <block type="text_print"></block>
-    </category>
-  </xml>
 
-';*/
 echo "<category name=\"Logic & Arithmetic\" colour=\"%{BKY_LOGIC_HUE}\">";
 foreach ($res as $re) 
 {
@@ -115,7 +82,7 @@ echo '<script>
          toolbox: document.getElementById("toolbox")});
     Blockly.Xml.domToWorkspace(document.getElementById("startBlocks"),programmingWorkspace);
   </script>';*/
-echo '</div>';
+//echo '</div>';
 
 echo '<div class="row">
 
@@ -155,4 +122,40 @@ var Workspace = Blockly.inject("blocklyDiv",
     }*/
 </script>
 </div> </form>';
+
+
+/*
+echo '
+<category name="Logic" colour="%{BKY_LOGIC_HUE}">
+      <block type="controls_if"></block>
+      <block type="logic_compare"></block>
+      <block type="logic_operation"></block>
+      <block type="logic_negate"></block>
+      <block type="logic_boolean"></block>
+    </category>
+    <category name="Loops" colour="%{BKY_LOOPS_HUE}">
+      <block type="controls_repeat_ext">
+        <value name="TIMES">
+          <block type="math_number">
+            <field name="NUM">10</field>
+          </block>
+        </value>
+      </block>
+      <block type="controls_whileUntil"></block>
+    </category>
+    <category name="Math" colour="%{BKY_MATH_HUE}">
+      <block type="math_number">
+        <field name="NUM">123</field>
+      </block>
+      <block type="math_arithmetic"></block>
+      <block type="math_single"></block>
+    </category>
+    <category name="Text" colour="%{BKY_TEXTS_HUE}">
+      <block type="text"></block>
+      <block type="text_length"></block>
+      <block type="text_print"></block>
+    </category>
+  </xml>
+
+';*/
 
