d54488a9dc80cd24a3741455419f2f4ff0393c75
karpad <karpad2@gmail.com>
2021-03-02 15:26:32 +0100

    program list

--- server_part/parts/programs.php
+++ server_part/parts/programs.php
@@ -29,7 +29,7 @@ $i=0;
         $i++;
     echo "<tr><td>$i</td><td>{$re["program_name"]}</td><td>{$re["room_name"]}</td>".
         "<td>".
-        "<a class=\"btn w-50 btn-outline-warning form-control\" href=\"index.php?mod=programming_interface&program_id={$re["program_id"]}\">Edit</a>".
+        "<a class=\"btn w-50 btn-outline-warning form-control\" href=\"index.php?mod=programming_interface&room_id={$re["room_id"]}program_id={$re["program_id"]}\">Edit</a>".
         "<a class=\"btn w-50 btn-outline-danger form-control\" href=\"index.php?programs&delete&program_id={$re["program_id"]}\">Delete</a>".
         "</td></tr>";
     }
