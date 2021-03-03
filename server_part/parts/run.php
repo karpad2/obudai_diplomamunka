e72defcdc8be2a278204deb0edf221ae6e3a6e41
karpad <karpad2@gmail.com>
2021-03-03 14:56:25 +0100

    link fix

--- server_part/parts/run.php
+++ server_part/parts/run.php
@@ -65,9 +65,9 @@ if (isset($_GET["run_mod"]))
     echo "<div class=\"col-md-4\">";
     echo "<h5 class=\"my-0 font-weight-normal\">Status:</h5>";
     echo "<div class=\"card-body\">
-            <h1 class=\"card-title pricing-card-title\"> 15 <small class=\"text-muted\">/ Devices</small></h1>
-            <h1 class=\"card-title pricing-card-title\"> 15 <small class=\"text-muted\">/ Program status</small></h1>
-            <h1 class=\"card-title pricing-card-title\"> 15 <small class=\"text-muted\">/ Program status</small></h1>";
+            <h5 class=\"card-title pricing-card-title\"> 15 <small class=\"text-muted\">/ active devices </small></h5>
+            <h5 class=\"card-title pricing-card-title\"> 15 <small class=\"text-muted\">/ Program status</small></h5>
+            <h5 class=\"card-title pricing-card-title\"> 15 <small class=\"text-muted\">/ Program status</small></h5>";
 
 
         /*    <ul class="list-unstyled mt-3 mb-4">
