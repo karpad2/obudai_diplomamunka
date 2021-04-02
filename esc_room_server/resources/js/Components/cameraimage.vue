0495cda00d7c87b1866afc718836888752e9c00c
karpad2 <karpad2@gmail.com>
2021-04-02 22:10:23 +0200

    what a lovely day right

---
+++ esc_room_server/resources/js/Components/cameraimage.vue
@@ -0,0 +1,8 @@
+<template>
+    <img alt="stream" v-bind:src="'http://10.147.17.234'" class="w-100" />
+</template>
+<script>
+import { BIconBatteryFull, BIconArrow90degDown, BIconBookmark } from 'bootstrap-icons-vue';
+
+
+</script>
