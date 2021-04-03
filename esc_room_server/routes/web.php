5262ea9e2113d4818bbe2b728370ec46df613b6d
karpad2 <karpad2@gmail.com>
2021-04-03 15:34:23 +0200

    route v2

--- esc_room_server/routes/web.php
+++ esc_room_server/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
@@ -12,7 +13,10 @@ use App\Http\Controllers\CamerasController;
 use App\Http\Controllers\EteamsController;
 use App\Http\Controllers\ProgramsController;
 use App\Http\Controllers\RoomController;
+use App\Models\Cameras;
+use App\Models\Programs;
 
+//use GuzzleHttp\Psr7\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,13 +29,13 @@ use App\Http\Controllers\RoomController;
 |
 */
 
+$id=Auth::id();
 
 Route::get('test',function()
 {
     return 'k';
 
 });
-
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -46,31 +50,45 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/rooms', function () {
-    return Inertia::render('rooms',[]);
+    return Inertia::render('rooms',[
+     'rooms'=> Room::where('user_id',Auth::id())->get()   
+    ]);
 })->name('rooms');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/room/{room_id}', function ($room_id) {
-    return Inertia::render('room',[]);
+    return Inertia::render('room',[
+        'room'=>Room::findorFail($room_id)
+    ]);
 })->name('room');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/devices', function () {
-    return Inertia::render('devices',[]);
+    return Inertia::render('devices',[
+        'devices'=>Devices::where(['user_id',Auth::id()])
+    ]);
 })->name('devices');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/device/{device_id}', function ($device_id) {
-    return Inertia::render('device',[]);
+    return Inertia::render('device',[
+        'device'=> Devices::findorFail($device_id)
+    ]);
 })->name('device');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/programs', function () {
-    return Inertia::render('programs',[]);
+    return Inertia::render('programs',[
+        'programs'=>Programs::where(['user_id',Auth::id()])
+    ]);
 })->name('programs');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/program/{program_id}', function ($device_password) {
-    return Inertia::render('program',[]);
+Route::middleware(['auth:sanctum', 'verified'])->get('/program/{program_id}', function ($program_id) {
+    return Inertia::render('program',[
+        'program'=>Programs::findorFail($program_id)
+    ]);
 })->name('program');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/cameras', function () {
-    return Inertia::render('cameras',[]);
+    return Inertia::render('cameras',[
+        'cameras'=>Cameras::where(['user_id',Auth::id()])
+    ]);
 })->name('cameras');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('add-user-device/{device_password}',function ($device_password){
@@ -88,3 +106,17 @@ Route::middleware(['auth:sanctum', 'verified'])->get('assign-device/{dev_id}/{ro
     Devices::findorFail($dev_id)->update(['room_id',$room_id]);
     return 'ok';
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->post('create-room',function (Request $request,$room_id){
+    
+    Room::create(["name"=>$request->name,"user_id"=>Auth::id()]);
+    return 'ok';
+});
+
+
+Route::middleware(['auth:sanctum', 'verified'])->post('program/{room_id}',function (Request $request,$room_id){
+    //Programs::create([]);
+    return 'ok';
+});
+
+
