9e9c605cf10744f40d86072e3f3b061e1bd9b828
karpad2 <karpad2@gmail.com>
2021-04-25 14:36:34 +0200

    route fix

--- esc_room_server/routes/api.php
+++ esc_room_server/routes/api.php
@@ -5,16 +5,6 @@ use App\Models\Devices;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 Route::get('/',function (Request $request){
     return 'test ok';
 });
