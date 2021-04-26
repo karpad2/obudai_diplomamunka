7d2ad25151e4577e363d3c0523a0e3c83b110be9
karpad2 <karpad2@gmail.com>
2021-04-26 18:10:58 +0200

    sql_fix

--- self_commit_push.sh
+++ self_commit_push.sh
@@ -1,12 +1,12 @@
 echo "self commit & push"
 
-mysqldump --opt -u root -p esp32project > ./db/esp32project.sql
-cp ./server_part/config.php ./server_part/config_sample.php 
-pandoc --extract-media ./docs/images -s index.docx -t markdown -o README.md
-pandoc --extract-media ./docs/images  -s index.docx -o ./docs/index.html --metadata title="Diplomamunka"
-pandoc -s index.docx -t  --pdf-engine=xelatex -o README.pdf
+mysqldump --opt -u root -p escape_room_lar > ./db/esp32project.sql
+#cp ./server_part/config.php ./server_part/config_sample.php 
+#pandoc --extract-media ./docs/images -s index.docx -t markdown -o README.md
+#pandoc --extract-media ./docs/images  -s index.docx -o ./docs/index.html --metadata title="Diplomamunka"
+#pandoc -s index.docx -t  --pdf-engine=xelatex -o README.pdf
 git add -A
-git commit -a -m "Auto commit "
+git commit -a -m "SQL DUMP"
 git push
 
 EXIT
