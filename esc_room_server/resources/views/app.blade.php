7457e4d0633d70f0245c89c9966ff126c5c3cfe2
karpad2 <karpad2@gmail.com>
2021-04-28 16:06:06 +0200

    cucc

--- esc_room_server/resources/views/app.blade.php
+++ esc_room_server/resources/views/app.blade.php
@@ -5,13 +5,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Escape Room Management') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        
+
+<link rel="apple-touch-icon" sizes="72x72" href="favicon.png" />
+<link rel="icon" type="image/png" sizes="192x192" href="favicon.png"  />
 
         <!-- Scripts -->
         @routes
