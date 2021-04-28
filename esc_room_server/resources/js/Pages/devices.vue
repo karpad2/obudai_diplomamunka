3bf92cd4c4b26d95a0a6d2ff248a82a4e65764d9
karpad2 <karpad2@gmail.com>
2021-04-28 15:57:24 +0200

    cucc

--- esc_room_server/resources/js/Pages/devices.vue
+++ esc_room_server/resources/js/Pages/devices.vue
@@ -8,7 +8,7 @@
 
         <div class="py-12">
 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-<div class="overflow-hidden shadow-xl sm:rounded-lg">
+<div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 <div>
 <div class="bg-white shadow-md rounded my-6">
 <table class="min-w-max w-full table-auto">
