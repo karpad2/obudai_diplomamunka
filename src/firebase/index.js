3635f670e32a32e0bb0e0bf85eb14164a91dcc90
Arpad Kovac <karpad2@gmail.com>
2021-08-18 13:10:14 +0200

    bruh

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,9 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/app-check';
 
 import firebaseCredentials from './credentials';
 firebase.initializeApp(firebaseCredentials.config);
+const appcheck=firebase.appCheck();
 
+appcheck.setTokenAutoRefreshEnabled();
 const FirebaseAuth = firebase.auth();
 const Firebase = firebase;
 
