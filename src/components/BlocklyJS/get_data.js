c519c214bc6570675c4cd3c8e2dfded5a3250ea3
karpad2 <karpad2@gmail.com>
2021-10-11 11:02:14 +0200

    hmm

--- src/components/BlocklyJS/get_data.js
+++ src/components/BlocklyJS/get_data.js
@@ -34,24 +34,7 @@ Blockly.defineBlocksWithJsonArray([
         "helpUrl": "",
         
     },
-]);//AS-Modifier
-
-/*
- "output": '',
-  
-Blockly.Extensions.register('assExtensions', function(){
-    this.setOnChange(function (changeEvent) {
-        var parent = this.getSurroundParent();
-        if(parent != null){
-            if(parent.toDevString().includes('select_from') || parent.toDevString().includes('all_join')){
-                this.getInput('as_oldName').setCheck("TABLE");
-            }
-            else{
-                this.getInput('as_oldName').setCheck(["freeinput", "aggregate_min", "aggregate_max", "aggregate_avg", "aggregate_count", "aggregate_sum", "CONDITIONCHOOSER"]);
-            }
-        }
-    })
-});*/
+]);
 Blockly.JavaScript['get_data'] = function(block) {
     var device = Blockly.JavaScript.statementToCode(block, 'device');
     var mode = Blockly.JavaScript.statementToCode(block, 'mode');
@@ -59,41 +42,3 @@ Blockly.JavaScript['get_data'] = function(block) {
     var code = `get_data(${device},${mode});`;
     return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
 };
-
-/*
-Blockly.JavaScript['controls_if'] = function(block) {
-  // If/elseif/else condition.
-  var n = 0;
-  var code = '', branchCode, conditionCode;
-  if (Blockly.JavaScript.STATEMENT_PREFIX) {
-    // Automatic prefix insertion is switched off for this block.  Add manually.
-    code += Blockly.JavaScript.injectId(Blockly.JavaScript.STATEMENT_PREFIX,
-        block);
-  }
-  do {
-    conditionCode = Blockly.JavaScript.valueToCode(block, 'IF' + n,
-        Blockly.JavaScript.ORDER_NONE) || 'false';
-    branchCode = Blockly.JavaScript.statementToCode(block, 'DO' + n);
-    if (Blockly.JavaScript.STATEMENT_SUFFIX) {
-      branchCode = Blockly.JavaScript.prefixLines(
-          Blockly.JavaScript.injectId(Blockly.JavaScript.STATEMENT_SUFFIX,
-          block), Blockly.JavaScript.INDENT) + branchCode;
-    }
-    code += (n > 0 ? ' else ' : '') +
-        'if (' + conditionCode + ') {\n' + branchCode + '}';
-    ++n;
-  } while (block.getInput('IF' + n));
-
-  if (block.getInput('ELSE') || Blockly.JavaScript.STATEMENT_SUFFIX) {
-    branchCode = Blockly.JavaScript.statementToCode(block, 'ELSE');
-    if (Blockly.JavaScript.STATEMENT_SUFFIX) {
-      branchCode = Blockly.JavaScript.prefixLines(
-          Blockly.JavaScript.injectId(Blockly.JavaScript.STATEMENT_SUFFIX,
-          block), Blockly.JavaScript.INDENT) + branchCode;
-    }
-    code += ' else {\n' + branchCode + '}';
-  }
-  return code + '\n';
-};
-
-*/
